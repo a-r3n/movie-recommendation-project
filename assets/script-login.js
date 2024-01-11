f71b428575e4c536fcf718f8439a97b94aee2479
Jonathan Santoso <jonathansantoso020@gmail.com>
2024-01-11 17:02:05 +0800

    added a functional login page and script

--- assets/script-login.js
+++ assets/script-login.js
@@ -1,37 +1,140 @@
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDgwe08Id9ABD-hItlCdcOiOflks4nvOhY",
-    authDomain: "movie-recommendation-pro-fc29b.firebaseapp.com",
-    projectId: "movie-recommendation-pro-fc29b",
-    storageBucket: "movie-recommendation-pro-fc29b.appspot.com",
-    messagingSenderId: "763942579046",
-    appId: "1:763942579046:web:10b966353190f847cef6a7"
+  apiKey: "AIzaSyCZaF8U9TyuYaGcL7_WU5la2GBxRkBK0Mc",
+  authDomain: "test-login-page-e7b09.firebaseapp.com",
+  projectId: "test-login-page-e7b09",
+  storageBucket: "test-login-page-e7b09.appspot.com",
+  messagingSenderId: "185374551631",
+  appId: "1:185374551631:web:eee5e67bce63810636c43a",
+  measurementId: "G-R9NDT6LY28"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Initializing Firebase
+var firebase = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth()
+const database = firebase.database()
 
-// Function to log in
-function login() {
-  var userEmail = document.getElementById("username").value;
-  var userPass = document.getElementById("password").value;
+// getting the warning field
+var freeField = document.getElementById('free');
 
-  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
-  .then((userCredential) => {
-      // Signed in 
-      var user = userCredential.user;
-      // You can redirect the user to a different page here
-      window.location.href = '/index-interface.html'; 
-  })
-  .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      document.getElementById("login-error").innerHTML = errorMessage;
-  });
+// Setting up the register function
+function register() {
+  // Getting all the input fields
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+  username = document.getElementById('username').value
+
+  // Validating input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    ('email or password is invalid.')
+    return
+  }
+  if (validate_username(username) == false) {
+    freeField.innerHTML = 'One or More Extra Fields is Outta Line!!'
+    return
+  }
+
+  // Auth
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      // Creating user variable
+      var user = auth.currentUser
+
+      // Adding user to Firebase Database
+      var database_ref = database.ref()
+
+      // Creating User data
+      var user_data = {
+        email: email,
+        username: username,
+        last_login: Date.now()
+      }
+
+      // Pushing user data to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+
+      // User creating message
+      freeField.innerHTML = 'User Created!!'
+    })
+    .catch(function (error) {
+      var error_code = error.code
+      var error_message = error.message
+      console.log(error_message)
+    })
 }
 
-// Prevent the form from submitting normally
-document.getElementById("login-form").addEventListener("submit", function(event){
-  event.preventDefault();
-  login();
-});
+// Setting up the login function
+function login() {
+  // Getting all input fields
+  username = document.getElementById("username").value;
+  email = document.getElementById('email').value
+  password = document.getElementById('password').value
+
+  // Validating input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    freeField.innerHTML = 'Email or Password is invalid'
+    return
+  }
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(function () {
+      // Craeting user variable
+      var user = auth.currentUser
+
+      // Adding user to Firebase Database
+      var database_ref = database.ref()
+
+      // Creating User data
+      var user_data = {
+        last_login: Date.now()
+      }
+
+      // Pushing user data Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+
+      // Login message
+      freeField.innerHTML = 'User Logged In!!'
+
+      // Redirecting to movie site after logging in
+      window.location.href = "http://127.0.0.1:5500/index-interface.html"
+
+    })
+    .catch(function (error) {
+      var error_code = error.code
+      var error_message = error.message
+      console.log(error_message)
+    })
+}
+
+// Validating the Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if (expression.test(email) == true) {
+    // if Email is valid
+    return true
+  } else {
+    // if Email is not valid
+    return false
+  }
+}
+
+function validate_password(password) {
+  // To ensure a password that is longer than 6 characters
+  if (password < 6) {
+    return false
+  } else {
+    return true
+  }
+}
+
+// validating the username field
+function validate_username(username) {
+  if (username == null) {
+    return false
+  }
+
+  if (username.length <= 0) {
+    return false
+  } else {
+    return true
+  }
+}
